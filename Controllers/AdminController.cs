0001fd5b1f14e4a94716157a3f601581cbdb29c4
DrowsyAlchemist <michaelGulidow@yandex.ru>
2025-03-16 22:39:57 +0300

    Add admin page (view, controller,  AdminModelView)

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.DataBase;
+using ToDoList.Logger;
+using ToDoList.Models;
+using ToDoList.ViewModels;
+
+namespace ToDoList.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserRepository _users;
+        private readonly TaskRepository _tasks;
+        private readonly AppLogger _logger;
+
+        public AdminController(UserRepository users, TaskRepository taskRepository, AppLogger appLogger)
+        {
+            _users = users;
+            _tasks = taskRepository;
+            _logger = appLogger;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            List<UserModel> users = await _users.GetAll();
+            List<TaskModel> tasks = await _tasks.GetAll();
+            return View(new AdminModel(users, tasks));
+        }
+    }
+}
---
+++ ViewModels/AdminModel.cs
@@ -0,0 +1,16 @@
+﻿using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public class AdminModel
+    {
+        public List<UserModel> Users { get; private set; }
+        public List<TaskModel> Tasks { get; private set; }
+
+        public AdminModel(List<UserModel> users, List<TaskModel> tasks)
+        {
+            Users = users;
+            Tasks = tasks;
+        }
+    }
+}
